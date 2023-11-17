d08c02dc47e53068e96518be3ba37ab974b8991c
ZhongCH <92720howard@gmail.com>
2023-11-17 23:43:57 +0800

    [Testing] Added circle test

---
+++ DrawerTests/Model/ShapeObjects/CircleTest.cs
@@ -0,0 +1,59 @@
+﻿using DrawerTests;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Drawer.ShapeObjects.Tests
+{
+    [TestClass]
+    public class CircleTest
+    {
+        [TestMethod]
+        public void GetCircleShapeType()
+        {
+            Circle circle = new Circle();
+            Assert.AreEqual(ShapeType.Circle, circle.Type);
+        }
+
+        [TestMethod]
+        public void GetCircleShapeName()
+        {
+            Circle circle = new Circle();
+            Assert.AreEqual("圓", circle.Name);
+        }
+
+        [TestMethod]
+        public void GetCircleInfo()
+        {
+            Circle circle = new Circle();
+            circle.Point1 = new Point(1, 1);
+            circle.Point2 = new Point(2, 2);
+            Assert.AreEqual("(1, 1), (2, 2)", circle.Info);
+        }
+
+        [TestMethod]
+        public void DrawCircle()
+        {
+            FakeGraphics graphics = new FakeGraphics();
+            Circle circle = new Circle();
+            circle.Draw(graphics);
+
+            Assert.AreEqual(0, graphics.NotifyDrawLineCount);
+            Assert.AreEqual(0, graphics.NotifyDrawRectangleCount);
+            Assert.AreEqual(1, graphics.NotifyDrawCircleCount);
+            Assert.AreEqual(0, graphics.NotifyDrawSelectBoxCount);
+        }
+
+        [TestMethod]
+        public void DrawSelectedCircle()
+        {
+            FakeGraphics graphics = new FakeGraphics();
+            Circle circle = new Circle();
+            circle.IsSelected = true;
+            circle.Draw(graphics);
+
+            Assert.AreEqual(0, graphics.NotifyDrawLineCount);
+            Assert.AreEqual(0, graphics.NotifyDrawRectangleCount);
+            Assert.AreEqual(1, graphics.NotifyDrawCircleCount);
+            Assert.AreEqual(1, graphics.NotifyDrawSelectBoxCount);
+        }
+    }
+}
